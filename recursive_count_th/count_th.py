a9dd11ec9d1f4b3c54eea4e1ff2455e2f5900b50
Jose Montero <josemariodev@gmail.com>
2020-02-21 11:38:46 -0600

    Passing tests for count_th

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -7,11 +7,19 @@ Your function must utilize recursion. It cannot contain any loops.
 
 def count_th(word):
 
+    # make sure word is at least 2 letters long
     if len(word) < 2:
         return 0
 
+    # if word contains th
     if word[:2] == "th":
 
         return 1 + count_th(word[1:])
+    else:
+        return 0 + count_th(word[1:])
 
-    return 0 + count_th(word[1:])
+
+# my own test
+print(count_th("thethethe"))
+# print(count_th("cat"))
+# print(count_th("I"))
